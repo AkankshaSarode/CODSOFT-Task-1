95f43ced9f0f8ec778702510d72490a87b2c027a
Akanksha Sarode27 <108226780+AkankshaSarode@users.noreply.github.com>
2024-07-26 14:01:24 +0530

    Add files via upload

---
+++ CODSOFT Level-1-landingpage/assest/js/style.js
@@ -0,0 +1,20 @@
+jQuery(".main-slider.owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dot: true,
+    autoplay: 3000,
+    responsive: {
+       0: {
+          items: 1
+       },
+       600: {
+          items: 1
+       },
+       1000: {
+          items: 1
+       },
+    }
+ });
+
+ 
